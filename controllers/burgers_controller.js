e0b8854212b9048d8d798197a5a3f349870fa2d4
iamjink <jinijinjin@gmail.com>
2020-04-01 10:12:50 -0700

    adding body-parser and method-override in server.js for devour button in index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,28 +12,24 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/burgers", function (req, res) {
+router.post("/burgers", function (req, res) {
     burger.createOne([
-        "burger_name", "devoured"
+        "burger_name"
     ], [
-        req.body.burger_name, req.body.devoured
-    ], function (result) {
-        res.json({id: result.insertId})
+        req.body.burger_name
+    ], function (data) {
+        res.redirect('/');
     });
 });
 
-router.put("/api/burgers/:id", function(req,res){
+router.put("/burgers/:id", function(req,res){
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
 
     burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function(result){
-        if(result.changedRows == 0){
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+        devoured: true
+    }, condition, function(data){
+      res.redirect("/");
     });
 });
 
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE in index.handlebars devour button
+app.use(methodOverride('_method'));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +23,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
+
 
 // var path = require ('path');
 // app.use(express.static(path.join(__dirname + '../public')));
